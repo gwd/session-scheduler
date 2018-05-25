34b1917efbdf758504f0052a735822e0f043bb4c
George Dunlap <george.dunlap@citrix.com>
2018-05-25 16:11:39 +0100

    HACK: Make single-threaded
    Just to get something live as quickly as possible, only allow one
    request to execute at a time.  Make a global lock and grab it on entry
    into ServeHTTP.
    
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- middleware.go
+++ middleware.go
@@ -1,19 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+)
 
-type Middleware []http.Handler
+// This has to be global because ServeHTTP cannot have a pointer receiver.
+var lock sync.Mutex
+
+type Middleware struct {
+	handlers []http.Handler
+}
 
 func (m *Middleware) Add(handler http.Handler) {
-	*m = append(*m, handler)
+	m.handlers = append(m.handlers, handler)
 }
 
 func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	
 	// Wrap the supplied ResponseWriter
 	mw := NewMiddlewareResponseWriter(w)
 
+	// HACK: Only allow a single access at a time for now
+	lock.Lock()
+	defer lock.Unlock()
+	
 	// Loop through all of the registered handlers
-	for _, handler := range m {
+	for _, handler := range m.handlers {
 		// Call the handler with our MiddlewareResponseWriter
 		handler.ServeHTTP(mw, r)
 
