bc269c47d5bb1b13a37da210a11d1c5f3cdc9e50
George Dunlap <george.dunlap@citrix.com>
2021-04-14 22:13:47 +0100

    ui: Add code to read the location from a cookie
    Add code to read the location from a cookie, if no location is
    specified in the URL.
    
    Add commented-out code to set the cookie when a specific location is
    requested (mostly for testing, but also as a place-holder until we get
    something better).
    
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- handle_schedule.go
+++ handle_schedule.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	//"time"
 
 	"github.com/julienschmidt/httprouter"
 
@@ -20,12 +21,27 @@ func HandleScheduleView(w http.ResponseWriter, r *http.Request, _ httprouter.Par
 
 	curLocationString := DefaultLocation
 	curLocationTZ := DefaultLocationTZ
+
 	if loc := r.FormValue("location"); loc != "" {
+		// If we've gone to a specific URL, use that location, and set a
+		// cookie to remember it.
 		tz, err := event.LoadLocation(loc)
 		if err == nil {
 			curLocationString = loc
 			curLocationTZ = tz
+			// http.SetCookie(w, &http.Cookie{
+			// 	Name:    locationCookieName,
+			// 	Value:   loc,
+			// 	Expires: time.Now().Add(time.Hour * 24 * 365)})
 		}
+	} else if cookie, err := r.Cookie(locationCookieName); err == nil {
+		// Otherwise, if there's a cookie, use that value.
+		tz, err := event.LoadLocation(cookie.Value)
+		if err == nil {
+			curLocationString = cookie.Value
+			curLocationTZ = tz
+		}
+
 	}
 
 	// FIXME: Handle the error
