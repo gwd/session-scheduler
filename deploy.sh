dff038beee0385d271364c325686b6bc8600fbd5
George Dunlap <george.dunlap@citrix.com>
2021-05-07 13:44:18 +0100

    deploy.sh: Take a directory argument
    ...to allow for test deployment.
    
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 SESSIONSDIR="session-scheduler"
+if [[ -n "$1" ]] ; then
+    SESSIONSDIR="$1"
+fi
 
 rsync -rvz assets templates session-scheduler run.sh xensched@xenbits:$SESSIONSDIR/
 ssh xensched@xenbits "mkdir -p $SESSIONSDIR/data"
