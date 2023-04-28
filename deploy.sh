6e93d4e815588153611e5747a084a2f1322f8242
George Dunlap <george.dunlap@cloud.com>
2023-04-28 14:34:55 +0100

    deploy.sh: Default to the test instance
    Signed-off-by: George Dunlap <george.dunlap@cloud.com>

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-SESSIONSDIR="session-scheduler"
+SESSIONSDIR="session-scheduler-test"
 if [[ -n "$1" ]] ; then
     SESSIONSDIR="$1"
 fi
 
-rsync -rvz assets templates session-scheduler xensched@xenbits:$SESSIONSDIR/
-ssh xensched@xenbits "mkdir -p $SESSIONSDIR/data"
+HOST=xensched@xenbits.xenproject.org
+
+rsync -rvz assets templates session-scheduler $HOST:$SESSIONSDIR/
+ssh $HOST "mkdir -p $SESSIONSDIR/data"
